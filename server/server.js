66de822f7cd2982add57679da0a9ec61d8e7fef9
Bishal Giri <bishal0922@gmail.com>
2024-03-13 02:04:38 -0500

    deployment changes

--- server/server.js
+++ server/server.js
@@ -78,6 +78,11 @@ app.get('/', (req, res) => {
   res.send('Welcome to the server');
 });
 
+app.get('/hello', (req, res) => {
+  res.send('Hello World');
+});
+
+
 // Start the server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
